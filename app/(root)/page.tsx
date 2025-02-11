154b6c02826e4a623efc52989ad7a1d40f47ed49
Abyan Dimas <abyandimas@Abyans-MacBook-Air.local>
2025-02-11 17:19:16 +0700

    update

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -238,6 +238,27 @@ export default async function IndexPage() {
           </Button>
         </Link>
       </section>
+      <section
+        id="certification"
+        className="container space-y-6 bg-slate-50 dark:bg-transparent py-10 my-14"
+      >
+        <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
+          <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+          Sertifikasi & Penghargaan
+          </h2>
+          <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
+          Berbagai sertifikasi dan penghargaan yang telah saya peroleh
+          </p>
+        </div>
+        <div className="mx-auto justify-center gap-4  md:w-full lg:grid-cols-3">
+          <ContributionCard contributions={featuredContributions} />
+        </div>
+        <Link href="/certification" className="flex justify-center">
+          <Button variant={"outline"} className="rounded-xl">
+            <Icons.chevronDown className="mr-2 h-4 w-4" /> View All
+          </Button>
+        </Link>
+        </section>
     </>
   );
 }
